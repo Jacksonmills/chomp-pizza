b87386e6d0897722554dc896f7c8c4a111f8f0c0
Jacksonmills <jacksonemills@gmail.com>
2024-06-25 22:10:20 -0500

    **Subject:** Fix typos and improve readability in homepage text
    **Body:**
    Corrected minor typographical errors and revised text for better clarity and flow on the homepage. Enhanced the readability of the introduction and promotional content, ensuring a smoother and more engaging user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
       <div className="max-w-lg grid gap-4 m-6 sm:m-12">
         <h1 className="text-4xl text-center mb-12 skew-x-12 skew-y-12">Chomp Pizza</h1>
         <p>
-          Hey, thanks for taking the time to come to this website about my pizza. I spend most of my time thinking about how to make better pizza, except for those moments when actual life hits you in the face, and usually snaps me out of it.
+          Hey, thanks for taking the time to come to this website about my pizza. I spend most of my time thinking about how to make better pizza, except for those moments when actual life hits you in the face and usually snaps me out of it.
         </p>
         <p>
-        As I’m wrote this, well, I guess I experienced one of those moments.
+          As I wrote this, well, I guess I experienced one of those moments.
         </p>
         <p>
-        Regardless of my intrusive thoughts, I’d like to make you a pizza sometime. So, if one of the pictures catches your eye, or maybe even you want to learn from someone with a little bit of experience (while we make pizza of course), send me an email. It’d be cool to hear from you.
+          Regardless of my intrusive thoughts, I’d like to make you a pizza sometime. So, if one of the pictures catches your eye, or maybe you even want to learn from someone with a little bit of experience (while we make pizza, of course), send me an email. It’d be cool to hear from you.
         </p>
         <div className="flex justify-between">
         <span className="flex gap-2 items-center">

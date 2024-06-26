4ba3f26209f3c8daa1ee49fe0014c88e4640843e
Jacksonmills <jacksonemills@gmail.com>
2024-06-25 22:06:36 -0500

    ### Enhancing homepage visuals and connectivity
    - Introduced `next/image` to optimize the turtle doodle and impove image loading.
    - Added new SVG image for the turtle doodle and removed the old icon import to better align with branding.
    - Updated heading style for a more dynamic visual appeal.
    - Incorporated social media and email buttons using `lucide-react` icons to improve user engagement and make it easier to connect.
    
    This update strives to make the homepage more visually appealing and enhance user interaction.
    
    Fixes #42

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import GridPattern from "@/components/GridPattern";
-import { cn } from "@/lib/utils";
+import Image from "next/image";
 import {
-  Turtle
+  Instagram,
+  Mail
 } from "lucide-react"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="grid place-content-center md:h-screen">
       <div className="max-w-lg grid gap-4 m-6 sm:m-12">
-        <h1 className={cn("text-4xl")}>Chomp Pizza</h1>
+        <h1 className="text-4xl text-center mb-12 skew-x-12 skew-y-12">Chomp Pizza</h1>
         <p>
           Hey, thanks for taking the time to come to this website about my pizza. I spend most of my time thinking about how to make better pizza, except for those moments when actual life hits you in the face, and usually snaps me out of it.
         </p>
@@ -18,9 +20,22 @@ export default function Home() {
         <p>
         Regardless of my intrusive thoughts, I’d like to make you a pizza sometime. So, if one of the pictures catches your eye, or maybe even you want to learn from someone with a little bit of experience (while we make pizza of course), send me an email. It’d be cool to hear from you.
         </p>
-        <span>
-<Turtle size={64} />
+        <div className="flex justify-between">
+        <span className="flex gap-2 items-center">
+          <span className="font-bold">-</span>
+          <span className="w-16 h-auto">
+            <Image src={`/chomp.svg`} alt="Turtle doodle" width={100} height={100} className="size-full object-contain" />
+          </span>
         </span>
+        <div className="flex gap-2">
+          <Button size="icon" variant="ghost" asChild>
+            <a href="https://www.instagram.com/chomp.pizza/" target="_blank" rel="noopener noreferrer"><Instagram /></a>
+          </Button>
+          <Button size="icon" variant="ghost" asChild>
+            <a href="mailto:travis@chomp.pizza"><Mail /></a>
+          </Button>
+        </div>
+        </div>
       </div>
       <GridPattern
         width={30}

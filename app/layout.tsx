987d0c14d95046fd1b2d66755cf6bb453aa7ad5d
Jacksonmills <jacksonemills@gmail.com>
2025-11-18 08:42:45 -0600

    Add Open Graph metadata and image for enhanced sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,24 @@ export const metadata: Metadata = {
   title: "Chomp Pizza",
   description:
     "I make pizza that I think is worth your $5. Address: 1710 W. 18th St. (Inside the Pink Line Station)",
+  metadataBase: new URL("https://chomp.pizza"),
+  openGraph: {
+    title: "Chomp Pizza",
+    description:
+      "I make pizza that I think is worth your $5. Address: 1710 W. 18th St. (Inside the Pink Line Station)",
+    url: "https://chomp.pizza",
+    siteName: "Chomp Pizza",
+    images: [
+      {
+        url: "https://dkoxmv7ca6.ufs.sh/f/s86AdQWJ310BpMxudO6dvCcgubOXQk7PYzNaLV8htZ5sSHfI",
+        width: 800,
+        height: 600,
+        alt: "A very good slice of pizza.",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
